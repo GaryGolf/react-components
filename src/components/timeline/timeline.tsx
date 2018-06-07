ddd9e378d4173a7d64e76f1e67c006e37ad642a4
GaryGolf <garygolf@mail.ru>
2018-06-07 18:06:15 +0300

    hide scroll bar

--- src/components/timeline/timeline.tsx
+++ src/components/timeline/timeline.tsx
@@ -4,18 +4,30 @@ import * as styles from './timeline.css';
 interface Props {};
 
 export default class TimeLine extends React.Component<Props, null> {
+  private handleScroll = (event:React.UIEvent<HTMLDivElement>) => {
+    const height = event.currentTarget.scrollHeight;
+    const top = event.currentTarget.scrollTop;
+    const client = event.currentTarget.clientHeight;
+    const { scrollHeight, scrollTop, clientHeight } = event.currentTarget; 
+    const scrollBottom = scrollHeight - clientHeight == scrollTop;
+    if (!scrollTop) console.log('top');
+    if (scrollBottom) console.log('bottom');
+  }
   render() {
     const rows = new Array(30)
       .fill('')
-      .map((x, idx) => <div key={idx}>{idx+ 'june'}</div>)
+      .map((x, idx) => <div key={idx}>{idx+ ' june'}</div>)
     return (
       <div>
-        today
-        tomorrow
-        weekend
-        june
-        <div className={styles.container}>
-          {rows}
+        <div>today</div>
+        <div>tomorrow</div>
+        <div>weekend</div>
+        <div>june</div>
+        <div className={styles.frame}>
+          <div className={styles.container}
+            onScroll={this.handleScroll}>
+            {rows}
+          </div>
         </div>
       </div>
     )
