a9c7c8ebb9cad435194905cde0d603ea20561d2d
GaryGolf <garygolf@mail.ru>
2018-06-22 16:08:27 +0300

    create accordion component

---
+++ src/components/accordion/accordion.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import * as s from './accordion.css'
+
+interface Props {
+
+}
+
+export default class Accordion extends React.Component<Props, null> {
+  render() {
+    return (
+      <div className={s.accordion}>
+      </div>
+    )
+  }
+}
