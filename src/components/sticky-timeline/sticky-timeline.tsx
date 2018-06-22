fd66a4b208f0e98ebd745ac93a2f2d6e788d870d
GaryGolf <garygolf@mail.ru>
2018-06-22 13:06:00 +0300

    try css sticky

--- src/components/sticky-timeline/sticky-timeline.tsx
+++ src/components/sticky-timeline/sticky-timeline.tsx
@@ -6,29 +6,42 @@ interface Props {}
 const styles = `
 .timeline {
   background: lightblue;
-  height: 100px;
+  height: 300px;
+  width: 300px;
+  position: relative;
+  margin: 0 auto;
   overflow: scroll;
+}
+.header {
+  position: sticky;
+  top: 0;
 }
 `;
 
 
 export default class StyckyTimeLine extends React.Component<Props, null> {
 
+  private container: HTMLDivElement;
+
   private renderSticky = () => {
     return new Array(30).fill(null).map((x,i) => (
         <div key={i}>qq</div>
     ))
   }
+
+  private renderContainer = () => {
+    return new Array(5).fill(null)
+      .map((x,i) => (
+        <div key={i}>
+          <h2 className="header">title + i</h2>
+          { this.renderSticky() }
+        </div>
+      ))
+  }
   render() {
     return (
-      <div className="timeline">
-        <StickyContainer className="events-day" id={'qq'}>
-					<Sticky className="sticky-header" stickyClassName="sticky-header-active" topOffset={-65}>
-						<h3 className="events-day_title">dayTitle</h3>
-					</Sticky>
-					<div className="sticky-spacer"></div>
-					{ this.renderSticky() }
-				</StickyContainer>
+      <div className="timeline" ref={el => this.container = el}>
+        {this.renderContainer()}
         <style>{styles}</style>
       </div>
     )
