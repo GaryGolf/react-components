8a891921b7905db6c36ffeebbed14cbb756dafed
GaryGolf <garygolf@mail.ru>
2018-06-23 16:46:19 +0300

    update render method

--- src/components/accordion/StickyAccordion.tsx
+++ src/components/accordion/StickyAccordion.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
-import AccordionHeader from './StickyHeader';
+import StickyHeader from './StickyHeader';
 import Waypoint from 'react-waypoint';
+import { uuid } from 'short-uuid';
 import * as s from './accordion.css'
 
 
 interface HeadElement {
-  index: number;
+  idx: number;
+  uuid: string;
   title: string;
   position: string;
   element: JSX.Element
@@ -34,17 +36,17 @@ export default class StickyAccordion extends React.Component<Props, State> {
     }
   }
   
-  private handleWaypointPositionChange = ({idx, position}) => {
+  private handleWaypointPositionChange = ({uuid, position}) => {
     this.setState((state:State) => {
-      const elements = state.elements.map(item => item.index == idx ? { ...item, position } : item)
+      const elements = state.elements.map(item => item.uuid == uuid ? { ...item, position } : item)
       return { elements };
     })
   }
 
   private handleHeaderClick = (event:React.MouseEvent<HTMLDivElement>) => {
-    const { idx } = event.currentTarget.dataset;
+    // const { idx } = event.currentTarget.dataset;
     // if (!this.container) return;
-    document.getElementById(idx).scrollIntoView({ block: 'start',  behavior: 'smooth' });
+    // document.getElementById(idx).scrollIntoView({ block: 'start',  behavior: 'smooth' });
   }
 
   private isHeading = (element:JSX.Element):boolean => 
@@ -57,10 +59,10 @@ export default class StickyAccordion extends React.Component<Props, State> {
       if (!this.isHeading(element)) return null;
 
       return ({
-        index: idx,
+        uuid: uuid(),
         title: element.props.children as string,
         position: null,
-        element
+        idx, element
       })
     }).filter(item => !!item)
 
@@ -71,12 +73,12 @@ export default class StickyAccordion extends React.Component<Props, State> {
 
     const above = this.state.elements
       .filter(e => ![Waypoint.inside, Waypoint.below].includes(e.position))
-      .map(e => <div key={`${e.title}+${e.index}`}>{e.title}</div>);
+      .map(e => <div key={e.uuid}>{e.title}</div>);
 
     const below = this.state.elements
       .filter(e => ![Waypoint.inside, Waypoint.above].includes(e.position))
       .map(e => (
-        <div key={e.title} data-idx={e.index} onClick={this.handleHeaderClick}>
+        <div key={e.uuid} onClick={this.handleHeaderClick}>
           {e.title}
         </div>
       ));
@@ -97,14 +99,23 @@ export default class StickyAccordion extends React.Component<Props, State> {
     const inside = React.Children.map(children, (item, idx) => {
       const element = item as JSX.Element;
 
-      if (!this.isHeading(element)) return item;
+      if (!this.isHeading(element)) return (
+        <element.type {...element.props} 
+          key={idx}
+          data-idx={idx}
+        />
+      );
+
+      const header = this.state.elements.find(e => e.idx == idx);
+      
+      if (!header) return null;
 
       return (
-        <AccordionHeader 
-          idx={idx}
-          onChange={this.handleWaypointPositionChange}>
-          {element}
-        </AccordionHeader>
+        <StickyHeader 
+          key={idx}
+          element={header}
+          onChange={this.handleWaypointPositionChange}
+        />
       );
     })
 
